16ae29527ee97cf287ae23f7877dc9ac96224101
claudine101 <ndayisabaclaudine01@gmail.com>
2022-11-15 18:30:16 +0200

    payemnent des services

--- src/screens/restaurant/RestaurantHomeScreen.jsx
+++ src/screens/restaurant/RestaurantHomeScreen.jsx
@@ -1,9 +1,11 @@
 import React, { useCallback, useState, useRef, useEffect } from "react";
 import { DrawerActions, useFocusEffect, useNavigation, useRoute } from "@react-navigation/native";
 import { COLORS } from "../../styles/COLORS";
+import { Text, View, useWindowDimensions, ImageBackground, StatusBar, StyleSheet, Image, TextInput, ScrollView, TouchableOpacity, FlatList, TouchableNativeFeedback } from "react-native";
+
 import { EvilIcons, MaterialIcons, AntDesign, Ionicons, MaterialCommunityIcons, FontAwesome, SimpleLineIcons, Feather, Octicons } from '@expo/vector-icons';
 import fetchApi from "../../helpers/fetchApi";
-import { Image, View, StyleSheet, Text, TouchableOpacity, TouchableNativeFeedback, TextInput, ScrollView, StatusBar, Modal, ActivityIndicator } from "react-native"
+// import { Image, View, StyleSheet, Text, TouchableOpacity, TouchableNativeFeedback, TextInput, ScrollView, StatusBar, Modal, ActivityIndicator } from "react-native"
 import Menu from "../../components/restaurant/main/Menu";
 import { HomeProductsSkeletons } from "../../components/ecommerce/skeletons/Skeletons";
 
